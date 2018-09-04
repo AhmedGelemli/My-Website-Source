2d9997b8dfe67179fe3178a037f904a849de4bf2
AhmedGelemli <35001559+AhmedGelemli@users.noreply.github.com>
2018-09-04 20:19:25 +0400

    added highlights.js

--- assets/js/jQueryforCode.js
+++
@@ -1,9 +0,0 @@
-jQuery.fn.highlight = function (str, className) {    
-    var regex = new RegExp(str, "gi");
-
-    return this.each(function () {
-        this.innerHTML = this.innerHTML.replace(regex, function(matched) {return "<span class=\"" + className + "\">" + matched + "</span>";});
-    });
-};
-
-$("xmp").highlight("<","highlight");
